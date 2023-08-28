ad15c97268205803f6e11eec71cadf13fcb81995
pavel-developer2001 <gaidukpavel18@gmail.com>
2023-08-28 14:32:55 +0300

    add cypress test

--- cypress/e2e/category.cy.tsx
+++ cypress/e2e/category.cy.tsx
@@ -1,8 +1,23 @@
 /// <reference types="cypress" />
 
 describe("category e2e tests", () => {
+  let data: any
+
+  beforeEach(() => {
+    cy.request("http://localhost:3001/categories").then((response) => {
+      data = response
+      console.info("BeforeEach Response: ", data)
+    })
+  })
   it("CRUD categories", () => {
     cy.visit("/")
     cy.get("[data-testid='category-input']").type("test category")
+    cy.get("[data-testid='category-btn']").click({ force: true })
+    cy.request({
+      url: `http://localhost:3001/categories`,
+      method: "POST",
+      body: { id: "100", title: "test category" },
+      failOnStatusCode: false,
+    })
   })
 })
